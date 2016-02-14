447bb3a2a11ae98abda8dd27d730baf3c0e02c99
hexagon6 <hexagon6@fet.li>
2016-02-14 16:54:48 +0100

    Move schedule.json to 32c3/schedule.json

--- c3-sessions.js
+++ c3-sessions.js
@@ -24,7 +24,7 @@ if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
     if(Talks.find().count() === 0){
-      var data = JSON.parse(Assets.getText("schedule.json"));
+      var data = JSON.parse(Assets.getText("32c3/schedule.json"));
 
       for(day = 0; day < data.schedule.conference.daysCount;  day++){
         var rooms = (data.schedule.conference.days[day]).rooms;
